46fadf9db0f82e1dc88ba72156d36dbb876af369
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2022-06-27 11:49:06 -0600

    Implementado routing para la API

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult geProducts()
+        {
+            return Ok('A');
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetProductByName(string name)
+        {
+            return Ok(name);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public double Price { get; set; }
+    }
+}
