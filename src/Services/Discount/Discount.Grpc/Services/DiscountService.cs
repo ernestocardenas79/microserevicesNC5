8de6504d5ae2cf3dd597c412e417e5c75644f4b2
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2022-06-29 18:35:24 -0600

    Feat: Implementado cascaron de discount service, y discount proto

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,40 @@
+﻿using Discount.Grpc.Protos;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(ILogger<DiscountService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CouponModel());
+        }
+
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CouponModel());
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CouponModel());
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new DeleteDiscountResponse());
+        }
+    }
+}
