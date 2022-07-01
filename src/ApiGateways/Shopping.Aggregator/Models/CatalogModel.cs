53b6f15dbb6bc7b28001f460734e385c3a446348
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2022-07-01 17:12:35 -0600

    Feat: aplicando el gateway implementando un aggregator para shopping

---
+++ src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Models
+{
+    public class CatalogModel
+    {
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+    }
+}
