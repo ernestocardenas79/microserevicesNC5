d51dafb432caa37f3676177169abc04ea6575b17
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2022-07-15 12:27:15 -0600

    Feat: Api for making orders

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguresOrders());
+                await orderContext.SaveChangesAsync();
+
+                logger.LogInformation("Seed database associated with context {DBContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguresOrders()
+        {
+            return new List<Order>
+            {
+                new Order(){ UserName="swn", FirstName ="Ernesto", LastName="Cardenas", EmailAddress="alguna@deporahi.com", Country="Mexico"}
+            };
+        }
+    }
+}
